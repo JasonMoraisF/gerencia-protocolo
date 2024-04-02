9bbcc113a61cc443f2755a71dc46869d33e60097
JasonMoraisF <jasoncamposfilho@hotmail.com>
2024-04-02 15:32:11 -0300

    initial commit

---
+++ src/main/java/br/com/gerenciaprotocolo/GerenciaProtocoloApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciaprotocolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaProtocoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaProtocoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gerenciaprotocolo/GerenciaProtocoloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciaprotocolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaProtocoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
