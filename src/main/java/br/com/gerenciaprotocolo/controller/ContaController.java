9d1406e6473b8f15577aa6c159777949f74dc6d1
Jason Morais <jasoncamposfilho@hotmail.com>
2024-04-14 14:00:19 -0300

    fix: java runtime compiler incompativel

--- src/main/java/br/com/gerenciaprotocolo/controller/ContaController.java
+++ src/main/java/br/com/gerenciaprotocolo/controller/ContaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import br.com.gerenciaprotocolo.model.Conta;
 import br.com.gerenciaprotocolo.repository.ContaRepository;
 
