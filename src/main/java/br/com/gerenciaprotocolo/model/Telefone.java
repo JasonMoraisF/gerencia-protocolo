bf193493ce4bf7144483d06c165adf98a8d47992
Jason de Morais <jasoncamposfilho@hotmail.com>
2024-05-19 21:40:43 -0300

    Fix: Relacao do Telefone

--- src/main/java/br/com/gerenciaprotocolo/model/Telefone.java
+++ src/main/java/br/com/gerenciaprotocolo/model/Telefone.java
@@ -1,13 +1,27 @@
 package br.com.gerenciaprotocolo.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Telefone")
+@JsonIdentityInfo(
+    generator = ObjectIdGenerators.PropertyGenerator.class,
+    property = "telefoneID")
 public class Telefone {
     
     @Id
@@ -21,21 +35,17 @@ public class Telefone {
     @Column(name = "DDD", nullable = false, length = 3)
     private String ddd;
 
-  
     @Column(name = "tipo_telefone", nullable = false, length = 11)
     @Enumerated(EnumType.STRING)
     private TipoTelefone tipoTelefone;
 
-
-    @JsonIgnore
-    @JsonManagedReference
     @ManyToOne
-    @JoinColumn(name = "cliente_id")
+    @JoinColumn(name = "cliente_id", nullable = true)
     private Cliente cliente;
 
-    @JsonBackReference
+    
     @ManyToOne
-    @JoinColumn(name = "titular_id")
+    @JoinColumn(name = "titular_id", nullable = true)
     private Titular titular;
 
     public Long getTelefoneID() {
