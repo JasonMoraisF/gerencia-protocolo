48c79fcd7f194d28573d1eef7eabef85408da466
Jason de Morais <jasoncamposfilho@hotmail.com>
2024-05-26 19:34:46 -0300

    Fix: GET do Protocolo e Situacao Protocolo falhando quando existe um protocolo

--- src/main/java/br/com/gerenciaprotocolo/service/ProtocoloService.java
+++ src/main/java/br/com/gerenciaprotocolo/service/ProtocoloService.java
@@ -2,7 +2,6 @@ package br.com.gerenciaprotocolo.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
